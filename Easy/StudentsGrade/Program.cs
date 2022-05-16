4774d16c5cdcd8a2140909e6016c6eea43b19621
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-16 11:24:12 +0300

    Add easy task

---
+++ Easy/StudentsGrade/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> grades = new List<int>();
+
+            for (int i = 0; i < gradesCount; i++)
+            {
+                int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+                grades.Add(gradesItem);
+            }
+
+            List<int> result = gradingStudents(grades);
+
+            Console.WriteLine(String.Join("\n", result));
+
+        }
+
+        private static List<int> gradingStudents(List<int> grades)
+        {
+            List<int> newGrades = new List<int>();
+            for (int i = 0; i < grades.Count; i++)
+            {
+                int currentGrade = grades[i];
+                int newValueGrade = 0;
+                if (currentGrade < 38)
+                {
+                    newValueGrade = currentGrade;
+                    newGrades.Add(newValueGrade);
+                    continue;
+                }
+
+                int j = 1;
+                int nextGrade = grades[i] + j;
+                while (nextGrade % 5 != 0)
+                {
+                    j++;
+                    nextGrade = grades[i] + j;
+                }
+                                
+                if (nextGrade - currentGrade < 3)
+                {
+                    newValueGrade = nextGrade;
+                    newGrades.Add(newValueGrade);
+                }
+                else
+                {
+                    newValueGrade = currentGrade;
+                    newGrades.Add(newValueGrade);
+                }
+            }
+
+            return newGrades;
+        }
+    }
+}
